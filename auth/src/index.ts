30486fd7eb19c960aa48575185ac6acbe84a9107
Shane O' Mahoney <shane@iom.com>
2020-09-19 09:39:40 +0100

    Init

---
+++ auth/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+import { currentUserRouter } from './routes/currentuser';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+
+const app = express();
+app.use(json());
+
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.use(currentUserRouter);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000.');
+});
