1a7e53cfa5dd8fa19257b8054f734ac43c587e22
Shane O' Mahoney <shane@iom.com>
2020-11-22 10:02:43 +0000

    Added new routes and tests

--- auth/src/routes/currentuser.ts
+++ auth/src/routes/currentuser.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@som-tickets/common';
 
 const router = express.Router();
 
--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,12 +2,11 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@som-tickets/common';
-
-import { currentUserRouter } from './routes/currentuser';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
+import { errorHandler, NotFoundError, currentUser } from '@som-tickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,11 +17,12 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
